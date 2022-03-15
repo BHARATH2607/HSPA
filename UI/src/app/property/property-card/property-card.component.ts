08ba866711d2942fe83147413fc938e009e7399d
BHARATH2607 <mbkumar2607@gmail.com>
2022-03-15 19:11:54 +0530

    new

--- UI/src/app/property/property-card/property-card.component.ts
+++ UI/src/app/property/property-card/property-card.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.css']
 })
+
 export class PropertyCardComponent implements OnInit {
 
-  Property : any = {
-    "Id" : 1,
-    "Name" : "Bharath",
-    "Type" : "2BHK",
-    "Price" : 120000
-  }
+  @Input() Property : any
 
   constructor() { }
 
